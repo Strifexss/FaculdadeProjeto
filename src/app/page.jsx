a4b3e2b7ba9a9749edcf3f60d935da9fcb5cbf33
Strifexss <rocombole0007@gmail.com>
2023-04-02 12:06:00 -0300

    #

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,16 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1>Teste</h1>
+      <div className={styles.container}> 
+        <div></div>
+        <div className={styles.logins}>
+          <input type="text" id="email" placeholder='E-mail' />
+          <input type="password" id="senha" placeholder='Senha' />
+          <button>
+            <h2>Entrar</h2>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
