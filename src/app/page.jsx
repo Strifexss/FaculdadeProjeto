509af5e4a1c0cc68850a409c6718d9d98ddc37b5
Strifexss <rocombole0007@gmail.com>
2023-04-02 12:39:33 -0300

    @

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import styles from './page.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import CidadeImagem from "./imgs/purpleCidade.webp"
+
 export default function Home() {
   return (
     <div className={styles.main}>
---
+++ src/app/registrar/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css"
+
+export default function registrar() {
+    return(
+        <div className={styles.main}>
+            <h1>Teste</h1>
+        </div>    
+    )
+}
