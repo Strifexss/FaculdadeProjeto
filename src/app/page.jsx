bfcb811eab0a8aa57e9effa7e3bfa53fba5264b7
Strifexss <rocombole0007@gmail.com>
2023-04-02 12:19:49 -0300

    #

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,25 @@
 import styles from './page.module.css'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import CidadeImagem from "./imgs/purpleCidade.webp"
 export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.container}> 
-        <div></div>
+        <div>
+          <Image
+            src={CidadeImagem}
+            width={500}
+            height={500}
+          />
+        </div>
         <div className={styles.logins}>
           <input type="text" id="email" placeholder='E-mail' />
           <input type="password" id="senha" placeholder='Senha' />
           <button>
             <h2>Entrar</h2>
           </button>
+          <Link href="/registrar"><p>Não tem uma conta? <span>Registre-se</span></p></Link>
         </div>
       </div>
     </div>
