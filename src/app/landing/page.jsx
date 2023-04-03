8001ba894a97ec6efbe722b26ea08da1dd97268f
Strifexss <rocombole0007@gmail.com>
2023-04-03 20:10:53 -0300

    #

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css"
 export default function landing() {
     return(
         <div className={styles.main}>
-            <h1>Logado</h1>
+            <h1>Logado!!!!!</h1>
         </div>
     )
 }
