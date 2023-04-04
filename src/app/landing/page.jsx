1d69c5ea088e7731cfb1500af4e4a6f78871725e
Strifexss <rocombole0007@gmail.com>
2023-04-04 12:48:13 -0300

    #

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -14,11 +14,16 @@ export default function landing() {
         }
     },[])
 
-
+    function deslogar() {
+        Cookies.removeItem("email")
+        window.alert("Deslogado")
+        push("/")
+    }
 
     return(
         <div className={styles.main}>
             <h1>Logado!!!!!</h1>
+            <button onClick={deslogar}>Deslogar</button>
         </div>
     )
 }
