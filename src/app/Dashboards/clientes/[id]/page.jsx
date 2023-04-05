a823e7f568db73934766cf7c531e1e8937d6f369
Strifexss <rocombole0007@gmail.com>
2023-04-05 12:42:24 -0300

    #

--- src/app/Dashboards/clientes/[id]/page.jsx
+++ src/app/Dashboards/clientes/[id]/page.jsx
@@ -26,7 +26,6 @@ export default function usuarios() {
    }
   )
 
-   function cadastrar() {
   function cadastrar() {
     const email = document.getElementById("email").value
     const peso = document.getElementById("peso").value
@@ -56,59 +55,6 @@ export default function usuarios() {
             <div className={styles.campo}>
                 <div className={styles.botoes}>
                     <button onClick={() => setCadastro(true)}><h2>Cadastrar Aluno</h2> </button>
-                    <button><h2>Deletar Aluno</h2> </button>
-                    <input type="text" placeholder="Pesquisar"/>
-                </div>
-                <div className={styles.container}>
-                    <div className={styles.containerCampos}>
-                        <h2>Nome</h2>
-                        <div className={styles.infos}>
-                            {data.map(x => {
-                                return(
-                                    <h3 key={x.id}>{x.nome}</h3>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className={styles.containerCampos}>
-                        <h2>Email</h2>
-                        <div className={styles.infos}>
-                        {data.map(x => {
-                                return(
-                                    <h3 key={x.id}>{x.email}</h3>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className={styles.containerCampos}>
-                        <h2>Telefone</h2>
-                        <div className={styles.infos}>
-                        {data.map(x => {
-                                return(
-                                    <h3 key={x.id}>{x.telefone}</h3>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className={styles.containerCampos}>
-                        <h2>Peso</h2>
-                        <div className={styles.infos}>
-                        {data.map(x => {
-                                return(
-                                    <h3 key={x.id}>{x.peso}</h3>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className={styles.containerCampos}>
-                        <h2>Altura</h2>
-                        <div className={styles.infos}>
-                        {data.map(x => {
-                                return(
-                                    <h3 key={x.id}>{x.altura}</h3>
-                                )
-                            })}
-                        </div>
                     <input type="text" placeholder="Pesquisar"/>
                 </div>
                 <div className={styles.cardsCampo}>
