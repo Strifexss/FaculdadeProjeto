e8f833fe9b496792cb6a679fa686f70d7bbb2177
Strifexss <rocombole0007@gmail.com>
2023-05-08 12:36:15 -0300

    #

--- src/app/Dashboards/aulas_aluno/page.jsx
+++ src/app/Dashboards/aulas_aluno/page.jsx
@@ -2,11 +2,13 @@
 
 import styles from "./page.module.css"
 import axios from "axios"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, useCallback } from "react"
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import Cookies from "js-cookies"
 export default function AulasAluno() {
 
+    const [infosModal, setInfosModal] = useState(false)
+    const [infos, setInfos] = useState([])
     const [adicionarModal, setAdicionarModal] = useState(false)
     const [dataAluno, setDataAluno] = useState([])
     const [dataAulas, setDataAulas] = useState([])
@@ -16,24 +18,36 @@ export default function AulasAluno() {
     const exercicio3 = useRef()
     const exercicio4 = useRef()
     const exercicio5 = useRef()
+    const dias_Edita = useRef()
+    const exercicio_1 = useRef()
+    const exercicio_2 = useRef()
+    const exercicio_3 = useRef()
+    const exercicio_4 = useRef()
+    const exercicio_5 = useRef()
 
-    const aulas =useQuery('buscarDadosAulas', async () => 
-    axios.post("https://planet-scale-database-connect.vercel.app/buscarAulasAluno", {
-        id_aluno: Cookies.getItem("id_aluno")
-    }).then(response => {
-        console.log(response)
-        setDataAulas(response.data)
-    })
-)
 
-    const { isLoading, error} =useQuery('buscarDadosAluno', async () => 
-    axios.post("https://planet-scale-database-connect.vercel.app/buscarAlunoEspecifico", {
-        id_aluno: Cookies.getItem("id_aluno")
-    }).then(response => {
+
+  useEffect(() => {
+    buscarAluno
+    buscarAulas
+  },[]) 
+
+  const buscarAulas = useQuery('buscarAulas', async () => 
+await  axios.post("https://planet-scale-database-connect.vercel.app/buscarAulasAluno", {
+    id_aluno: Cookies.getItem("id_aluno")
+}).then(response => {
     console.log(response)
-    setDataAluno(response.data)
-}) 
-)
+    setDataAulas(response.data)
+}))
+
+const buscarAluno = useQuery('buscarAluno', async () => 
+await axios.post("https://planet-scale-database-connect.vercel.app/buscarAlunoEspecifico", {
+    id_aluno: Cookies.getItem("id_aluno")
+}).then(response => {
+console.log(response)
+setDataAluno(response.data)
+}))
+   
 
     function adicionar() {
         axios.post("https://planet-scale-database-connect.vercel.app/criarAulaAluno", {
@@ -49,6 +63,31 @@ export default function AulasAluno() {
         }).catch(error => {
             console.log(error)
         })
+    }
+
+    function handleInfos(id) {
+        console.log(id)
+        setInfos(dataAulas.filter(x => {return x.id == id}))
+        console.log(infos)
+        setInfosModal(!infosModal)
+    }
+
+    function editar() {
+        console.log(exercicio_1.current.textContent)
+        axios.post("https://planet-scale-database-connect.vercel.app/ModificarAulasAluno", {
+            id: infos[0].id,
+            dia_semana: dias_Edita.current.textContent,
+            exercicio_1: exercicio_1.current.textContent,
+            exercicio_2: exercicio_2.current.textContent,
+            exercicio_3: exercicio_3.current.textContent,
+            exercicio_4: exercicio_4.current.textContent,
+            exercicio_5: exercicio_5.current.textContent
+        }).then(response => {
+            console.log(response)
+        }).catch(err => {
+            console.log(err)
+        })
+
     }
 
     return(
@@ -71,13 +110,8 @@ export default function AulasAluno() {
                 {
                     dataAulas.map(x => {
                         return(
-                            <div className={styles.card} key={x.id}>
-                                <header>
+                            <div className={styles.card} key={x.id} onClick={() => handleInfos(x.id)}>
                                 <h1>{x.dia_semana}</h1>
-                                </header>
-                                <section>
-                                <h2>{x.exercicio_1}</h2>
-                                </section>
                             </div>
                         )
                     })
@@ -101,6 +135,33 @@ export default function AulasAluno() {
                         <input type="text" placeholder="Exercicio 4" ref={exercicio4}/>
                         <input type="text" placeholder="Exercicio 5" ref={exercicio5}/>
                         <button onClick={adicionar}>Enviar</button>
+                    </div>
+                }
+                {
+                    infosModal && 
+                    <div className={styles.infosModal}>
+                        <header>
+                            <button onClick={() => setInfosModal(false)}>Fechar</button>
+                            <button onClick={editar}>Editar </button>
+                        </header>
+                        <h1 contentEditable ref={dias_Edita}>{infos[0].dia_semana}</h1>
+                        <div className={styles.infosCampo}>
+                            <section>
+                            <h2 contentEditable ref={exercicio_1}>{infos[0].exercicio_1}</h2>
+                            </section>
+                            <section>
+                            <h2 ref={exercicio_2} contentEditable>{infos[0].exercicio_2}</h2>
+                            </section>
+                            <section>
+                            <h2 ref={exercicio_3} contentEditable>{infos[0].exercicio_3}</h2>
+                            </section>
+                            <section>
+                            <h2 ref={exercicio_4} contentEditable>{infos[0].exercicio_4}</h2>
+                            </section>
+                            <section>
+                            <h2 ref={exercicio_5} contentEditable>{infos[0].exercicio_5}</h2>
+                            </section>
+                       </div>
                     </div>
                 }
             </div>
